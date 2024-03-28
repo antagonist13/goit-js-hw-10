52322fd1d43201acc346fd12296ee86254d9c7e4
antagonist13 <1703shakayaroslaw@gmail.com>
2024-03-28 21:52:26 +0200

    task 2

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -3,24 +3,48 @@ import "izitoast/dist/css/iziToast.min.css";
 
 const delayLabel = document.querySelector(".form").firstElementChild
 delayLabel.classList.add('delay')
-delayLabel.lastElementChild.classList.add('delayInput')
+const delayInput = delayLabel.lastElementChild
+delayInput.classList.add('delayInput')
+
 const createbtn = document.querySelector("button")
 createbtn.classList.add("create-btn")
 
-createbtn.addEventListener("click", (() => {
+const fulfilledInput = document.querySelector('input[value="fulfilled"]');
+const rejectedInput = document.querySelector('input[value="rejected"]');
 
+
+createbtn.addEventListener("click", ((event) => {
+    event.preventDefault()
+    let status
+    setTimeout(() => {
+        if (fulfilledInput.checked) { status = true };
+        if (rejectedInput.checked) { status = false };
+
+        promiseCreator(status).then(smile => {
+        iziToast.success({
+            message: `${smile} Fulfilled promise in ${delayInput.value}ms`,
+            position: 'topRight'
+        });
+            console.log(`${smile} Fulfilled promise in ${delayInput.value}ms`);
+        })
+        promiseCreator(status).catch(smile => {
+        iziToast.error({
+            message: `${smile} Rejected promise in ${delayInput.value}ms`,
+            position: 'topRight'
+        });
+            console.log(`${smile} Rejected promise in ${delayInput.value}ms`);
+        });
+    }, delayInput.value)
 }))
 
-function createPromise(delay, isActive, value) {
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (isActive) {
-        resolve(value);
+function promiseCreator (status) {
+            const promise = new Promise((resolve, reject) => {
+      if (status) {
+        resolve('✅');
       } else {
-        reject(value);
+        reject('❌');
       }
-    }, delay);
-  });
-
+    });
+  ;
   return promise;
 }
